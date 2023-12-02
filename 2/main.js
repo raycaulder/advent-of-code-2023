e342cb42186a0f879684408164c17c2613e271b1
Ray Caulder <rcaulder@redventures.com>
2023-12-02 11:33:49 -0500

    updated day 2 readability

--- 2/main.js
+++ 2/main.js
@@ -10,22 +10,27 @@ async function part1() {
   let sum = 0;
 
   inputIterator((line) => {
+    // Parse the game ID
     const gameId = parseInt(line.split(":")[0].substring(5));
-    const gameData = line.split(":")[1].trim().split(";");
+    // Parse each hand
+    const handData = line.split(":")[1].trim().split(";");
 
     let isPossible = true;
-    gameData.forEach((game) => {
-      game.split(",").forEach((color) => {
+    handData.forEach((game) => {
+      // Split the colors and iterate
+      handData.split(",").forEach((color) => {
         const cubeKeyValue = color.trim().split(" ");
+        // Grab possibility as long as game is still possible
         isPossible = isPossible
           ? parseInt(cubeKeyValue[0]) <= constraints[cubeKeyValue[1]]
           : false;
       });
     });
+
+    // Is the game possible, if yes then add id
     if (isPossible) sum += parseInt(gameId);
-    else {
-    }
   }).then(() => {
+    // Answer
     console.log(sum);
   });
 }
@@ -38,9 +43,7 @@ async function part2() {
   };
 
   let sum = 0;
-
   inputIterator((line) => {
-    const gameId = parseInt(line.split(":")[0].substring(5));
     const handData = line.split(":")[1].trim().split(";");
 
     const minimumCubes = {
@@ -52,18 +55,21 @@ async function part2() {
     handData.forEach((hand) => {
       hand.split(",").forEach((color) => {
         const cubeKeyValue = color.trim().split(" ");
+
+        // Set the minimum cubes value if new hand has a greater amount of cubes
         minimumCubes[cubeKeyValue[1]] =
           minimumCubes[cubeKeyValue[1]] < parseInt(cubeKeyValue[0])
             ? parseInt(cubeKeyValue[0])
             : minimumCubes[cubeKeyValue[1]];
       });
     });
-
+    // Reduce list of values of object to get product of values, then add
     sum += Object.values(minimumCubes).reduce((a, b) => a * b);
   }).then(() => {
+    // Answer
     console.log(sum);
   });
 }
 
-// part1();
+part1();
 part2();
